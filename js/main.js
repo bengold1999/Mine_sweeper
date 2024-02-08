163cf4b756fa482a5c1cc6ea82179b787c8a8ce8
Ben Goldberger <126246230+bengold1999@users.noreply.github.com>
2024-02-08 19:16:56 +0200

    upload_version_3

--- js/main.js
+++ js/main.js
@@ -4,14 +4,13 @@ var gBoard
 const MINE = '☼'
 var gElselected
 var MARK = '🚩'
-var gNumberOfMark
+var gNumberOfMark 
 var gnumLives =3
 var gGame = {
     isOn: true,
+    isOver: false,
     shownCount: 0,
-    markedCount: 0,
-    secsPassed: 0,
-    mineCounts: 0
+    markedCount: 0
 }
 
 
@@ -25,30 +24,22 @@ function sizeLevel(level, mines) {
     onInit()
 }
 function onPlay() {
-    
+    if(gGame.isOver)
+        return
+
     gBoard = copyMat(gBoard)
     renderBoard(gBoard)
 
-    // if()
-    if(gnumLives===0){
-        onInit()
-        gnumLives=3
-    }
-
-
+   
     
     
     
     if (!gGame.isOn) {
-        // copyMat(gBoard)
-        // renderBoard(gBoard)
         gGame.isOn = true
         var modal = document.querySelector('.gameover')
         modal.style.display = 'none'
     }
-    gGame.mineCounts = 0
-    gGame.shownCount = 0
-    gNumberOfMark = 0
+
     var smiley = document.querySelector('.smile')
     smiley.innerText = '😀'
     var modal2 = document.querySelector('.try')
@@ -57,7 +48,7 @@ function onPlay() {
 
    
    
-    //    if(gnumLives===0)gnumLives=3
+
              
     
 
@@ -66,10 +57,20 @@ function onPlay() {
 }
 
 function onInit() {
-    // onPlay()
+    gGame.isOver = false
+    gGame.shownCount = 0
+    gGame.markedCount = 0
+    gnumLives=3
     gBoard = buildBoard()
     renderBoard(gBoard)
-    gnumLives=3
+    
+    var modal2 = document.querySelector('.try')
+    modal2.style.display = 'none'
+    if( modal2.style.display === 'none'){
+        gnumLives=3
+        onPlay()
+        gNumberOfMark = 0
+    }
 
 
 }
@@ -112,21 +113,24 @@ function renderBoard(board) {
         strHTML += '<tr>'
         for (var j = 0; j < board[0].length; j++) {
             const cell = board[i][j]
-            const className = cell.isShown ? 'show' : ''
+            var className = '';
+
+            
 
             if (cell.isMine === true) {
                 cell.minesAroundCount = MINE
-
+                cell.isShown = false
+                className = 'mine '
             }
-            strHTML += `<td data-i=${i} data-j=${j} onClick = "onCellClicked(this,${i},${j})" oncontextmenu="onCellMarked(event,this,${i},${j})" class="${className}">${cell.minesAroundCount}</td> `
-            // if(board[i][j].minesAroundCount===0){
-            //     board[i][j].innerText=''
-            // }
+
+            className += cell.isShown ? 'show' : ''
+
+            strHTML += `<td data-i=${i} data-j=${j} onClick = "onCellClicked(this,${i},${j})" oncontextmenu="onCellMarked(event,this,${i},${j})" class="${className}">${cell.minesAroundCount === 0 ? '' : cell.minesAroundCount}</td> `
+            
         }
         strHTML += '</tr>'
-    }
 
-    console.log(strHTML)
+    }
 
     const elBoard = document.querySelector('.board')
 
@@ -139,21 +143,29 @@ function renderBoard(board) {
 
 
 function onCellClicked(elCell, i, j) {
+    if(elCell.classList.contains('marked')) return
     if (!gGame.isOn) return
-    console.log(elCell)
     const cell = gBoard[i][j]
+
+    if(cell.isMine && gGame.shownCount === 0){
+        gBoard = buildBoard()
+        renderBoard(gBoard)
+        elCell = document.querySelector(`[data-i="${i}"][data-j="${j}"]`)
+        return onCellClicked(elCell, i, j)
+    }
+    
 
     // var cellsMines = cell.isMine
     if (cell.isShown) return
     if (cell.isMarked) return
 
     cell.isShown = true
-    gGame.shownCount++
+    if (!cell.isMine)
+        gGame.shownCount++
     elCell.classList.add('show')
 
     if (cell.isMine) {
         showAllMines()  
-        gGame.mineCounts++
         gnumLives--
         gGame.isOn=false
         checkGameOver()
@@ -175,7 +187,7 @@ function onCellClicked(elCell, i, j) {
 function onCellMarked(event, elCell, i, j) {
     gNumberOfMark++
 
-    event.preventDefault();
+    event.preventDefault()
 
     var isMarked = elCell.classList.contains('marked')
     var isShown = elCell.classList.contains('show')
@@ -188,7 +200,6 @@ function onCellMarked(event, elCell, i, j) {
         elCell.classList.add('marked')
         elCell.innerHTML = MARK
     }
-    checkGameOver()
 }
 
 function setMinesNegsCount(board, rowIdx, colIdx) {
@@ -200,8 +211,6 @@ function setMinesNegsCount(board, rowIdx, colIdx) {
             if (i === rowIdx && j === colIdx) continue
             if (j < 0 || j >= board[0].length) continue
             var currCell = board[i][j]
-            // console.log(i,j)
-            // console.log(currCell)
             if (currCell.isMine === true) count++
         }
     }
@@ -253,35 +262,56 @@ function randomMine(board, numsMines) {
 
 
 function checkGameOver() {
-    var modal = document.querySelector('.gameover')
     var modal2 = document.querySelector('.try')
     var smiley = document.querySelector('.smile')
+    console.log(gGame.shownCount)
     if(!gGame.isOn){
         smiley.innerText = '🤯'
         
         modal2.style.display = 'block'
     }
-    if  (gGame.mineCounts === gLevel.MINES || gnumLives === 0) {
+    if  (gnumLives === 0) {
+        gameover(false)
+    }
+    else if (gGame.shownCount === (gLevel.SIZE ** 2 - gLevel.MINES)) {
+        gameover(true)
+    }
+    
+
+}
+
+
+function gameover(win){
+    
+
+    var modal = document.querySelector('.gameover')
+    var modal2 = document.querySelector('.try')
+    var smiley = document.querySelector('.smile')
+
+
+
+    if(win){
 
         gGame.isOn = false
+        gGame.isOver = true
+        modal.style.display = 'block'
+        smiley.innerText = '😎'
+        var mineElements = document.querySelectorAll('.mine')
+        for(var i = 0; mineElements.length > i; i++){
+            mineElements[i].classList.add('marked')
+            mineElements[i].innerHTML = MARK
+            
+        }
+        
+    }else{
 
+        gGame.isOn = false
+        gGame.isOver = true
         modal.style.display = 'block'
         modal2.style.display = 'none'
 
         
         smiley.innerText = '🤯'
-        
-
 
     }
-    else if (gGame.shownCount === (gLevel.SIZE ** 2 - gLevel.MINES + gGame.mineCounts) && gNumberOfMark === gLevel.MINES) {
-        gGame.isOn = false
-        modal.style.display = 'block'
-        var smiley = document.querySelector('.smile')
-        smiley.innerText = '😎'
-
-    }
-    // console.log(gGame.isOn)
-
 }
-
